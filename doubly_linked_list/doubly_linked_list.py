77b30deb77d4c53d654f82be784071137c442e3a
Brianna Workman <brianna.workman@hotmail.com>
2020-03-23 12:46:20 -0500

    adds delete head and tail

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -71,7 +71,9 @@ class DoublyLinkedList:
     Returns the value of the removed Node."""
 
     def remove_from_head(self):
-        pass
+        value = self.head.value
+        self.delete(self.head)
+        return value
 
     """Wraps the given value in a ListNode and inserts it 
     as the new tail of the list. Don't forget to handle 
@@ -93,7 +95,9 @@ class DoublyLinkedList:
     Returns the value of the removed Node."""
 
     def remove_from_tail(self):
-        pass
+        value = self.tail.value
+        self.delete(self.tail)
+        return value
 
     """Removes the input node from its current spot in the 
     List and inserts it as the new head node of the List."""
@@ -127,7 +131,8 @@ class DoublyLinkedList:
             self.tail = self.tail.prev
             node.delete()
         # if regular node
-        node.delete()
+        else:
+            node.delete()
     """Returns the highest value currently in the list"""
 
     def get_max(self):
